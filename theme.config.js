9948b4c06533a704d981b9392a53cc216d2d4dea
Hakimei <54173539+Hakimei@users.noreply.github.com>
2022-08-29 18:01:01 +0700

    change config

--- theme.config.js
+++ theme.config.js
@@ -16,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="EVO design team Documentation" />
+      <meta name="og:description" content="EVO design team Documentation" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="twitter:image" content="public/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="og:title" content="EVO design team Documentation" />
+      <meta name="og:image" content="public/og.png" />
+      <meta name="apple-mobile-web-app-title" content="EVO Design" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
